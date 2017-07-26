838caa09f234c179ab44cd29293d06edf0541e1d
Ryan Loebs <obsidianx@gmail.com>
2017-07-25 17:05:04 -0700

    Fix batch file, re-build minified version

---
+++ build/build.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+@echo Compiling TypeScript...
+cmd /c tsc ../src/tokenizer.ts ../src/bbCodeParseTree.ts ../src/bbTag.ts ../src/bbCodeParser.ts
+@echo Minifying...
+cmd /c uglifyjs ../src/tokenizer.js ../src/bbCodeParseTree.js ../src/bbTag.js ../src/bbCodeParser.js --output bbCodeParser.min.js
